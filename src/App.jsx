df71a4a304c0296d8bdd7fc340af5f1e44f9953a
Sebastian Ethington <sashi.ethington@gmail.com>
2026-02-05 07:05:54 -0700

    updated App.jsx code to  initialize cube state and build the color selector function. Display a warning when user tries to change color of center square started color const in the colorPicker.jsx file to build the picker layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,31 @@
-// layout for the whole app
+// layout of the app 
+
+import { useState } from "react";
+import { initialCubeState } from "./features/solver/cubeState";
+
+export default function App() {
+  const [cubeData, setCubeData] = useState(initialCubeState);
+  const [selectedColor, setSelectedColor] = useState("white");
+
+  const handleStickerClick = (face, index) => {
+    if (index === 4) {
+      console.warn("Centers cannot be changed!");
+      return;
+    }
+
+    setCubeData((previousCubeData) => {
+      return {
+        ...previousCubeData,
+        [face]: previousCubeData[face].map((color, i) => 
+          i === index ? selectedColor : color
+        )
+      };
+    });
+  };
+
+  return (
+    <div className="app">
+      <h1>Rubik's Solver</h1>
+    </div>
+  );
+}
--- src/components/colorPicker.jsx
+++ src/components/colorPicker.jsx
@@ -1,1 +1,3 @@
 // color panel for the user to select the colors for the cube
+
+const colors = ["white", "orange", "green", "red", "blue", "yellow"];
